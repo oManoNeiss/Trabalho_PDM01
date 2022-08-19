cea669f11e630da7e88e0bc707e5350119972070
Eduardo Antonio Neiss <100316865+oManoNeiss@users.noreply.github.com>
2022-08-19 11:10:14 -0300

    Add files via upload

---
+++ Trabalho_PDM/script.js
@@ -0,0 +1,9 @@
+function start() {
+
+    let nome = prompt('Qual seu nome?')
+    let idade = prompt('Qual sua idade?')
+    let city = prompt('Em que cidade você mora?')
+
+    console.log(nome + ' ' + idade + ' ' + city)
+
+}
